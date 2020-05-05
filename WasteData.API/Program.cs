fd16c7ae75ffc9f4aae9e55c2f29ef6b9743f966
Stijn <yonas@dvl-sanitair.be>
2020-05-05 15:51:00 +0000

    Added SeriLog

--- WasteData.API/Program.cs
+++ WasteData.API/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
 
 namespace WasteData.API
 {
@@ -13,11 +15,30 @@ namespace WasteData.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+           .Enrich.FromLogContext()
+           .WriteTo.Console()
+           .WriteTo.File(new RenderedCompactJsonFormatter(), "/logs/log.ndjson")
+           .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- WasteData.App/Services/HttpService.cs
+++ WasteData.App/Services/HttpService.cs
@@ -6,27 +6,36 @@ using System.Text;
 using System.Threading.Tasks;
 using WasteData.App.Services.Interfaces;
 using WasteData.App.Services.DTO;
+using Microsoft.Extensions.Logging;
 
 namespace WasteData.App.Services
 {
     public class HttpService : IHttpService
     {
+        private readonly ILogger<IHttpService> _logger;
+        public HttpService(ILogger<IHttpService> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task<string> GetCountryByIpAddress(string ipAddress) 
         {
             try
             {
-                using (var client = new HttpClient())
-                using (var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.ipstack.com/{ipAddress}?access_key=c67733d05edce7b32b2d410b68c24d53"))
-                using (var response = await client.SendAsync(request))
-                {
-                    response.EnsureSuccessStatusCode();
-                    var content = await response.Content.ReadAsStringAsync();
-                    var ipAddressLookup = JsonConvert.DeserializeObject<IpAddressLookupDto>(content);
-                    return ipAddressLookup.CountryCode;
-                }
+                using var client = new HttpClient();
+                using var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.ipstack.com/{ipAddress}?access_key=c67733d05edce7b32b2d410b68c24d53");
+                using var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                var ipAddressLookup = JsonConvert.DeserializeObject<IpAddressLookupDto>(content);
+
+                _logger.LogInformation($"GetCountryByIpAddress - IP:{ipAddress} - Country:{ipAddressLookup.CountryCode}");
+
+                return ipAddressLookup.CountryCode;
             }
             catch (Exception ex) 
             {
+                _logger.LogError("GetCountryByIpAddress", ex);
                 return string.Empty;
             }
         }
